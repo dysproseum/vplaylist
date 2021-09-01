953dd625d02141bb77c4f2cc576b11e624671284
David Younker <dysproseum@gmail.com>
2021-09-01 19:49:19 -0400

    Generate one or all collections at a time

--- generate.php
+++ generate.php
@@ -2,7 +2,25 @@
 
 require_once 'include/bootstrap.php';
 
-foreach ($collections as $name => $items) {
+$ffmpeg = 'ffmpeg';
+
+$machine_names[] = array();
+if (isset($argv[1])) {
+	$machine_names[] = $argv[1];
+}
+else if (isset($argv[1]) && $argv[1] == '--all') {
+	$ffmpeg .= ' -y ';
+	foreach ($collections as $name => $items) {
+		$machine_names[] = $name;
+	}
+}
+else {
+	print "Usage: php generate.php [collection_id]\n\n";
+	print " --all	Regenerate thumbnails from all collections.\n";
+	exit;
+}
+
+foreach ($machine_names as $name) {
 
 	$out_dir = THUMBS_PATH . $name;
 	exec('mkdir -p ' . $out_dir);
@@ -12,7 +30,7 @@ foreach ($collections as $name => $items) {
 		$input = '/mnt/' . $item['filename'];
 		$output = $out_dir . '/' .  basename($input, '.mp4') . '.jpg';
 
-		$ffmpeg = 'ffmpeg -ss 00:00:01.00 -i "' . $input . '" -vf \'scale=320:320:force_original_aspect_ratio=decrease\' -vframes 1 "' . $output . '"';
+		$ffmpeg.= ' -ss 00:00:01.00 -i "' . $input . '" -vf \'scale=320:320:force_original_aspect_ratio=decrease\' -vframes 1 "' . $output . '"';
 
 		print("\n" .$ffmpeg);
 		exec($ffmpeg);
