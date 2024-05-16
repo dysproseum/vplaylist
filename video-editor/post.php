e8cbd5a34907b2c09feb2ff15ffae12509bdf5e5
dysproseum <dysproseum@gmail.com>
2024-05-16 13:50:32 +0000

    Video import abstractions

--- video-editor/post.php
+++ video-editor/post.php
@@ -1,29 +1,42 @@
 <?php
 
-$urls = [];
+// Load paths from config.
+$config_path = dirname(__FILE__) . "/../config.php";
+if (file_exists($config_path)) {
+  require_once($config_path);
+}
+else {
+  print "Could not load host paths from config_path.\n";
+  print "Expected: $config_path\n";
+  exit;
+}
 
 if (!isset($_REQUEST['video1'])) {
   exit;
 }
 
-$urls[] = $_REQUEST['video1'];
+$url = $_REQUEST['video1'];
+
+// Sanitize url.
+$url = filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED);
+if (!$url) {
+  exit('Invalid URL');
+}
 
 // @todo dependency check on admin page
 // check for needed file write locations
 
-// @todo pull paths from config file.
-$p = '/uploads/video-editor/links.txt';
+// Set file path.
+$video_editor_dir = $conf['video_dir'] . "/video-editor";
+$p = $video_editor_dir . "/links.txt";
+
 $handle = fopen($p, 'a');
 if ($handle) {
-
-  foreach ($urls as $url) {
-    fputs($handle, $url . PHP_EOL);
-  }
+  fputs($handle, $url . PHP_EOL);
   fclose($handle);
-  
 }
 else {
-  // error opening the file.
+  exit('Error writing links');
 }
 ?>
 
@@ -38,7 +51,7 @@ else {
 
       <h4><a href="index.php">Add Another Video</a></h4>
 
-      <h4><a href="/vplaylist/index.php?collection=video_editor">View Uploaded Videos</a></h4>
+      <h4><a href="/vplaylist/index.php?collection=imported_videos">View Uploaded Videos</a></h4>
     </form>
   </div>
 </div>
