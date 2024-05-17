51292ebfc81b20120eae69171d78d6b8aff88e79
dysproseum <dysproseum@gmail.com>
2024-05-17 02:07:21 +0000

    Account for vertical videos

--- scripts/collect_mp4.sh
+++ scripts/collect_mp4.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd download
+
 for i in *.*
 do
 	echo "$i"
--- scripts/convert_mp4_html5.sh
+++ scripts/convert_mp4_html5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # based on https://gist.github.com/yellowled/1439610
 IN=$1
-OUT=mp4/$(echo $1 | sed 's/^\(.*\)\.[a-zA-Z0-9]*$/\1/')
+OUT=../mp4/$(echo $1 | sed 's/^\(.*\)\.[a-zA-Z0-9]*$/\1/')
 
 # webm
 #ffmpeg -i "$IN" -f webm -vcodec libvpx -acodec libvorbis -ab 128000 -crf 22 -s 640x360 "$OUT.webm"
@@ -10,7 +10,19 @@ OUT=mp4/$(echo $1 | sed 's/^\(.*\)\.[a-zA-Z0-9]*$/\1/')
 #ffmpeg -i "$IN" -acodec aac -strict experimental -ac 2 -ab 128k -vcodec libx264 -preset slow -f mp4 -crf 22 -s 640x360 "$OUT.mp4"
 
 # mp4 plays in chrome
-ffmpeg -i "$IN" -n -acodec aac -b:a 400k -vcodec libx264 -preset medium -f mp4 -crf 18 -pix_fmt yuv420p -qcomp 0.8 -s 640x360 -x264-params ref=4 -profile:v baseline -level 3.1 -movflags +faststart "$OUT.mp4"
+#ffmpeg -i "$IN" -n -acodec aac -b:a 400k -vcodec libx264 -preset medium -f mp4 -crf 18 -pix_fmt yuv420p -qcomp 0.8 -s 640x360 -x264-params ref=4 -profile:v baseline -level 3.1 -movflags +faststart "$OUT.mp4"
 
 # ogg (if you want to support older Firefox)
 #ffmpeg2theora $IN -o $OUT.ogv -x 640 -y 360 --videoquality 5 --audioquality 0  --frontend
+
+# mp4 with aspect ratio check
+WIDTH=$(ffprobe -loglevel error -select_streams v:0 -show_entries stream=width -of csv=s=,:p=0 "$IN")
+HEIGHT=$(ffprobe -loglevel error -select_streams v:0 -show_entries stream=height -of csv=s=,:p=0 "$IN")
+
+if [ $WIDTH -lt $HEIGHT ]; then
+  # vertical
+  ffmpeg -loglevel quiet -i "$IN" -n -acodec aac -b:a 400k -vcodec libx264 -preset medium -f mp4 -crf 18 -pix_fmt yuv420p -qcomp 0.8 -s 360x640 -x264-params ref=4 -profile:v baseline -level 3.1 -movflags +faststart "$OUT.mp4"
+else
+  # horizontal
+  ffmpeg -loglevel quiet -i "$IN" -n -acodec aac -b:a 400k -vcodec libx264 -preset medium -f mp4 -crf 18 -pix_fmt yuv420p -qcomp 0.8 -s 640x360 -x264-params ref=4 -profile:v baseline -level 3.1 -movflags +faststart "$OUT.mp4"
+fi
