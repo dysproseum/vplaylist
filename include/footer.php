2924186387ced7293cee763b9d8bcd55264973e1
David Younker <dysproseum@gmail.com>
2024-08-26 16:34:43 -0400

    Update footer links

--- include/footer.php
+++ include/footer.php
@@ -3,7 +3,7 @@
 			<a href="/vplaylist/index.php">Home</a>
 			<a href="/vplaylist/install.php">Add collection</a>
 			<a href="/vplaylist/video-editor/">Add video</a>
-			Part of the 🐧<a href="/conversations/open-source-desktop.html">Open Source Desktop</a>
+			Part of the 🐧<a href="https://dysproseum.com/open-source-desktop.html">Open Source Desktop</a>
 			powered by <a target="_blank" href="https://github.com/dysproseum/vplaylist">vplaylist on github</a>
 			</h6>
 		</div>
